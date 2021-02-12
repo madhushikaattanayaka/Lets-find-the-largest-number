028a842926c425337ffd995b798a97ce115fee9c
Madhushika Sewmini Attanayaka <64924916+madsew@users.noreply.github.com>
2021-02-12 17:21:28 +0530

    Add files via upload

---
+++ Find the largest num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{ int A,B;
+  printf("Enter A\n");
+  scanf("%d", &A );
+  printf("Enter B\n");
+  scanf("%d", &B );
+  
+  
+ if (A > B)
+    printf("A is the largest\n");
+ else
+    printf("B is the largest"); 
+
+
+
+
+
+
+}
